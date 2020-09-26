f8ec6f13fe79df6cd3651867667c3e132213e108
ken-santoso <ken.santoso@xero.com>
2020-09-26 18:21:48 +1000

    Initial Commit

---
+++ src/RoomDataBFF/Startup.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+
+namespace RoomDataBFF
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddAuthentication("Bearer").AddJwtBearer("Bearer", o =>
+            {
+                o.Authority = "https://localhost:5001";
+                o.TokenValidationParameters = new TokenValidationParameters{
+                    ValidateAudience = false
+                };
+            });
+            services.AddAuthorization(x =>
+            {
+                x.AddPolicy("ApiScope", p =>
+                    {
+                        p.RequireAuthenticatedUser();
+                        p.RequireClaim("scope", "api1");
+                    });
+            });
+        }
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers().RequireAuthorization("ApiScope"); });
+        }
+    }
+}
